b59cd8022de26eff42d5631d5a3713fa5d1e29df
Brandon Barker <brandon-barker@users.noreply.github.com>
2014-08-19 10:47:21 +0200

    Create floatThead.js

---
+++ floatThead.js
@@ -0,0 +1,49 @@
+/**
+ * floatThead wrapper for AngularJS
+ * @version v0.0.1 - 2014-08-19 
+ * @link https://github.com/brandon-barker/ng-floatThead
+ * @author Brandon Barker
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('floatThead', [])
+        .directive('floatThead', ['$timeout', '$log', floatThead]);
+    
+    function floatThead ($timeout, $log) {
+        // Usage:
+        // Specify float-thead on any table element and optionally pass through a floatThead options object to initialize the library.
+        // Optionally specify ng-model to have the directive watch any objects for changes and call 'reflow' on floatThead.   
+        // You can also manually trigger a reflow by triggering an event on the table element called 'update', eg: $('.table').trigger('update');
+        var directive = {
+            require: '?ngModel',
+            link: link,
+            restrict: 'A'
+        };
+        return directive;
+
+        function link(scope, element, attrs, ngModel) {
+            $(element).floatThead(scope.$eval(attrs.floatThead));
+
+            if (ngModel) {
+                // Set $watch to do a deep watch on the ngModel (collection) by specifying true as a 3rd parameter
+                scope.$watch(attrs.ngModel, function () {                    
+                    $(element).floatThead('reflow');
+                }, true);
+            } else {
+                $log.info('floatThead: ngModel not provided!');
+            }
+
+            element.bind('update', function () {                
+                $timeout(function() {
+                    $(element).floatThead('reflow');
+                }, 0);
+            });
+
+            element.bind('$destroy', function() {
+                $(element).floatThead('destroy');
+            });
+        }
+    }
+})();
