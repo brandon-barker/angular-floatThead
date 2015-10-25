2b1b01fd6754db62b8941f8df57a06dc6a24a59d
Brandon Barker <skinty@gmail.com>
2015-10-25 11:02:30 +0200

    Added ability to enable/disable the directive via scope variable to fix #8

--- angular-floatThead.js
+++ angular-floatThead.js
@@ -1,50 +1,65 @@
 /**
  * floatThead wrapper for AngularJS
- * @version v0.0.1 - 2014-08-19 
- * @link https://github.com/brandon-barker/ng-floatThead
+ * @version v0.1.0 - 2015-10-25
+ * @link https://github.com/brandon-barker/angular-floatThead
  * @author Brandon Barker
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('floatThead', [])
-        .directive('floatThead', ['$timeout', '$log', floatThead]);
-    
-    function floatThead ($timeout, $log) {
-        // Usage:
-        // Specify float-thead on any table element and optionally pass through a floatThead options object to initialize the library.
-        // Optionally specify ng-model to have the directive watch any objects for changes and call 'reflow' on floatThead.   
-        // You can also manually trigger a reflow by triggering an event on the table element called 'update', eg: jQuery('.table').trigger('update');
-        var directive = {
-            require: '?ngModel',
-            link: link,
-            restrict: 'A'
-        };
-        return directive;
+  angular
+    .module('floatThead', [])
+    .directive('floatThead', ['$timeout', '$log', floatThead]);
 
-        function link(scope, element, attrs, ngModel) {
-            jQuery(element).floatThead(scope.$eval(attrs.floatThead));
+  function floatThead($timeout, $log) {
+    // Usage:
+    // Specify float-thead on any table element and optionally pass through a floatThead options object to initialize the library.
+    // Optionally specify ng-model to have the directive watch any objects for changes and call 'reflow' on floatThead.
+    // You can also manually trigger a reflow by triggering an event on the table element called 'update', eg: jQuery('.table').trigger('update');
+    var directive = {
+      require: '?ngModel',
+      scope: {
+        floatTheadEnabled: '='
+      },
+      link: link,
+      restrict: 'A'
+    };
+    return directive;
 
-            if (ngModel) {
-                // Set $watch to do a deep watch on the ngModel (collection) by specifying true as a 3rd parameter
-                scope.$watch(attrs.ngModel, function () {                    
-                    jQuery(element).floatThead('reflow');
-                }, true);
-            } else {
-                $log.info('floatThead: ngModel not provided!');
-            }
+    function link(scope, element, attrs, ngModel) {
+      var isEnabled = (scope.floatTheadEnabled === true);
 
-            element.bind('update', function () {                
-                $timeout(function() {
-                    jQuery(element).floatThead('reflow');
-                }, 0);
-            });
+      if (isEnabled) {
+        jQuery(element).floatThead(scope.$eval(attrs.floatThead));
+      }
 
-            element.bind('$destroy', function() {
-                jQuery(element).floatThead('destroy');
-            });
+      scope.$watch('floatTheadEnabled', function (newVal) {
+        if (newVal === true) {
+          jQuery(element).floatThead(scope.$eval(attrs.floatThead));
+        } else {
+          jQuery(element).floatThead('destroy');
         }
+      });
+
+      if (ngModel) {
+        // Set $watch to do a deep watch on the ngModel (collection) by specifying true as a 3rd parameter
+        scope.$watch(attrs.ngModel, function () {
+          jQuery(element).floatThead('reflow');
+        }, true);
+      } else {
+        $log.info('floatThead: ngModel not provided!');
+      }
+
+      element.bind('update', function () {
+        $timeout(function () {
+          jQuery(element).floatThead('reflow');
+        }, 0);
+      });
+
+      element.bind('$destroy', function () {
+        jQuery(element).floatThead('destroy');
+      });
     }
+  }
 })();
